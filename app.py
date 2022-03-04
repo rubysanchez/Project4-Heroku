0cb786aa96d899b1c06af8f71909752523852431
ToniEdwards <mstonielt@gmail.com>
2022-03-04 09:26:58 -0600

    Working Commit

--- app.py
+++ app.py
@@ -1,29 +1,28 @@
 
 import pandas as pd
+import pickle 
 from flask import Flask, render_template, request, redirect, jsonify, make_response
-
-def prediction(data):
-    return data
 
 app = Flask(__name__)
 
+prediction_model = pickle.load(open("resources/machine_learning_model.pkl","rb"))
 @app.route("/")
 def index():
     #home page for info and navigation
     return render_template("index.html")
 
 @app.route("/data")
-def index():
+def data():
     #SQL lite
     return ("#jsonified response")
 
 @app.route("/model/", method=["POST", "GET"])
-def index():
-    #make the prediction
+def model():
+    #model the prediction
     if request.method == "POST":
         input = request.form
-    pred = prediction(input)
-    return render_template("index.html", pred = pred)
+    pred = prediction_model.predict(input)
+    return render_template("index.html", pred=pred)
 
 if __name__ == '__main__':
     app.run()
---
+++ resources/prediction.py
@@ -0,0 +1,2 @@
+def prediction(data):
+    return data
