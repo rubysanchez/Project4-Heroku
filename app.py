1403ec33d80ab8263fa0d88853e0ec98031f106f
ToniEdwards <mstonielt@gmail.com>
2022-03-06 14:13:24 -0600

    Commiting the viz.html endpoint

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def data():
 
 @app.route("/model/", methods=["POST", "GET"])
 def model():
-    #model the prediction
+    #retrieve user inputs
     if request.method == "POST":
         input_age = request.form["age"]
         input_race = request.form["ethnicity"]
@@ -36,8 +36,24 @@ def model():
         input_gender = request.form["gender"]
         input_stage = request.form["stage"]
         input_site = request.form["site"]
-    factors = [input_age,input_race, input_income, input_gender, input_stage, input_site]
-    pred = prediction_model.predict(factors)
+        input_type = request.form["type"]
+    
+    factors = [input_age,input_race, input_income, input_gender, input_stage, input_site, input_type]
+
+    #model the prediction
+    prediction_data = pd.DataFrame()
+    prediction_data['Age'] = int(factors[0])
+    prediction_data[f'Race_{factors[1]}']=1
+    prediction_data[f'Median_Household_Income_{factors[2]}']=1
+    prediction_data[f'Gender_{factors[3]}']=1
+    prediction_data[f'Cancer_Stage_{factors[4]}']=1
+    prediction_data[f'Cancer_Site_{factors[5]}']=1
+    prediction_data[f'Cancer_Type_{factors[6]}']=1
+    
+    #make the prediction
+    pred = prediction_model.predict(prediction_data)
+
+    #return the prediction
     return render_template("index.html", pred=pred)
 
 if __name__ == '__main__':
